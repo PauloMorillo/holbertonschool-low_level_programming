4f9dd84f54fae0a54c93b1488a248e28de96fe05
PauloMorillo <pauloan@hotmail.com>
2019-06-20 19:47:58 +0000

    created script 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	signed int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n <= '8')
+		{
+			putchar(',');
+		}
+		n = n + 1;
+	}
+	putchar('\n');
+	return (0);
+}
