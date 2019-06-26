502a861a5023acb0473eda6d13d830d3008dbef6
PauloMorillo <pauloan@hotmail.com>
2019-06-26 00:47:11 +0000

    fixed script with space

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,52 +1,45 @@
 #include "holberton.h"
 /**
  * times_table - returns 1 if is alpha else returns 0.
- *
- *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
 {
 	int a = 0;
 	int b = 0;
-	/*int co = 0;*/
+	int co = 0;
 	int valo = 0;
-	int ult;
-	int dece;
+	int ult, dece;
 
 	while (a <= 9)
 	{
 		while (b <= 9)
 		{
 			valo = a * b;
-			if(valo < 10){
-				_putchar(valo + '0');
-				if (b != 9)
+			if (valo < 10)
+			{
+				if (co > 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
-			}else
+				_putchar(valo + '0');
+				co = co + 1;
+			} else
 			{
 				ult = valo % 10;
 				dece = ((valo - ult) / 10);
+				_putchar(',');
+				_putchar(' ');
 				_putchar(dece + '0');
 				_putchar(ult + '0');
-				if (b != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				/*_putchar(dece + '0');
-				 *_putchar(ult + '0');*/
 			}
 			b = b + 1;
 		}
 		_putchar('\n');
 		b = 0;
+		co = 0;
 		a = a + 1;
 	}
-
 }
