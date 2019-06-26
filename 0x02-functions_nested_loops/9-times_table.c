c96fbd21a3555ea782a74880532b79cbbf3f3892
PauloMorillo <pauloan@hotmail.com>
2019-06-26 00:29:12 +0000

    created script excercise 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,45 @@
  */
 void times_table(void)
 {
-	int a = '0';
-	int b = '0';
-	while (a < '10')
+	int a = 0;
+	int b = 0;
+	/*int co = 0;*/
+	int valo = 0;
+	int ult;
+	int dece;
+
+	while (a <= 9)
 	{
+		while (b <= 9)
+		{
+			valo = a * b;
+			if(valo < 10){
+				_putchar(valo + '0');
+				if (b != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}else
+			{
+				ult = valo % 10;
+				dece = ((valo - ult) / 10);
+				_putchar(dece + '0');
+				_putchar(ult + '0');
+				if (b != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				/*_putchar(dece + '0');
+				 *_putchar(ult + '0');*/
+			}
+			b = b + 1;
+		}
+		_putchar('\n');
+		b = 0;
+		a = a + 1;
+	}
 
 }
