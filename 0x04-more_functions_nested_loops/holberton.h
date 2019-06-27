ff22521b116e9885a5e72b734efa21df1be4aaa3
PauloMorillo <pauloan@hotmail.com>
2019-06-27 20:13:59 +0000

    created script exercise 7

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,9 @@
 #ifndef _holberton_h_
 #define _holberton_h_
 
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
 void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
