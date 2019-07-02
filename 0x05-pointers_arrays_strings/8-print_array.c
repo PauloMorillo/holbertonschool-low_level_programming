63e7af6b13b14ae265eb4c5ed8c4bdfa3868e973
PauloMorillo <pauloan@hotmail.com>
2019-07-02 23:40:24 +0000

    created script excercise 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - print all strings with step 2
+ * @a: input strings
+ * @n: input length of array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	/*int c = 0;*/
+	int b = 0;
+
+	while (b < n)
+	{
+		if (b == n - 1)
+		{
+			printf("%d\n",a[b]);
+		}
+		else
+		{
+			printf("%d, ",a[b]);
+		}
+		b = b + 1;
+	}
+}
