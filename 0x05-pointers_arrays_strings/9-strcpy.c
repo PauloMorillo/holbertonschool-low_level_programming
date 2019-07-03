f7f3796ffa7fdf5b539a88fd881140f1bb4c63a7
PauloMorillo <pauloan@hotmail.com>
2019-07-03 01:15:25 +0000

    created script excercise 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - print all posix
+ * @dest: input strings
+ * @src: input length of array
+ * Return: Pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	/*int c = 0;*/
+	int a = 0;
+	int b = 0;
+
+	while (src[a] != '\0')
+	{
+		a = a + 1;
+	}
+	/*printf("%d longitud",a);*/
+	while (b < a)
+	{
+		if (b == (a - 1))
+		{
+			dest[b] = '\n';
+		}
+		else
+		{
+			dest[b] = src[b];
+		}
+		b = b + 1;
+	}
+	return (dest);
+}
