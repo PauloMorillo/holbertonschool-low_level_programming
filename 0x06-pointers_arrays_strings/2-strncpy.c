c4939747c0804eb55c85a70694e9ab774849c486
PauloMorillo <pauloan@hotmail.com>
2019-07-04 20:13:36 +0000

    fixed change the conditional

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copy a string between another
+ * @dest: first input word
+ * @src: second input word
+ * @n: input length to concatenated from src
+ * Return: concatenated answer in first string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	int a2 = 0;
+	int a3 = 0;
+
+	/*n = n / 8;*/
+	while (dest[a] != '\0')
+	{
+		a = a + 1;
+	}
+	while (src[a2] != '\0')
+        {
+                a2 = a2 + 1;
+        }
+	while (a3 < n)
+	{
+		if (a3 <= a2)
+		{
+			dest[a3] = src[a3];
+		}
+		else
+		{
+			dest[a3] = 0;
+		}
+		a3 = a3 + 1;
+	}
+	return (dest);
+}
