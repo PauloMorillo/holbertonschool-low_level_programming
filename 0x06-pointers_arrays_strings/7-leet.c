2a7b96dee58d315483cd8605ba3a710e3d0e0ced
PauloMorillo <pauloan@hotmail.com>
2019-07-05 00:44:03 +0000

    Created script for excercise 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s1: first input word to string to encode
+ *
+ *
+ * Return: answer of encode
+ */
+
+char *leet(char *s1)
+{
+	int a = 0;
+	int b = 0;
+	char c[11] = "aeotlAEOTL";
+	int e[11] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+
+	/*n = n / 8;*/
+	while (s1[a] != '\0')
+	{
+		while (b < 5)
+		{
+			if (s1[a] == c[b])
+			{
+				s1[a] = e[b];
+			}
+			else
+			{
+				s1[a] = s1[a];
+			}
+			b = b + 1;
+		}
+		b = 0;
+		a = a + 1;
+	}
+	return (s1);
+}
