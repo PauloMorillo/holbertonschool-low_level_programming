232421b39a2452efec09db8b6819255f6b193b6f
PauloMorillo <pauloan@hotmail.com>
2019-07-08 23:27:42 +0000

    fixed script with null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * _strchr - locates a character in a string
  * @s: input string
  * @c: input character to locates in string
  *
- * Return: matrix filled.
+ * Return: matric with character or NULL
  */
 
 char *_strchr(char *s, char c)
 {
 	int a = 0;
-	int b = 0;
 
 	while (s[a] != '\0')
 	{
 		if (s[a] == c)
 		{
-			b = b + 1;
-			break;
+			return (s = s + a);
 		}
 		a = a + 1;
 	}
-	s = s + (a);
-	return (s);
+	return (NULL);
 }
