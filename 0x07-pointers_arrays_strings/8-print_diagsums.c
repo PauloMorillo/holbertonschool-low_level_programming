7d69afb1ce6a8f14ad4e061509aba97c88327608
PauloMorillo <pauloan@hotmail.com>
2019-07-09 01:33:40 +0000

    created scrip for excercise 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * print_diagsums - print sum of diagonals of a matrix
  * @a: input matrix
- *
+ * @size: input size of matrix
  *
  * Return: matrix filled.
  */
@@ -12,16 +12,16 @@ void print_diagsums(int *a, int size)
 {
 	int b = 0;
 	int c = 0;
+	int d1 = 0;
+	int d2 = 0;
 
-	while (b < 8)
+	while (b < (size * size))
 	{
-		while (c < 8)
-		{
-			_putchar(a[b][c]);
-			c = c + 1;
-		}
-		_putchar('\n');
-		c = 0;
-		b = b + 1;
+		d1 = d1 + a[b];
+		/*d2 = d2 + a[c];*/
+		b = b + 1 + size;
+		c = c + (size - 1);
+		d2 = d2 + a[c];
 	}
+	printf("%d, %d\n", d1, d2);
 }
