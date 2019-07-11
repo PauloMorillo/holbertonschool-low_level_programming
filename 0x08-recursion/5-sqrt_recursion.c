40365c0500e22a1a15989f2e756b300bb865fa6f
familia <pauloan@hotmail.com>
2019-07-10 22:38:55 -0500

    fixed with betty style

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,33 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _sqrt_recursion - function calculate x root square
- * look_multiples - look the multiples to calculate the root
- * @n: input number to calculate root
- * @a: input first digit to count 
+ *  _sqrt_recursion - funtion to calculate the factorial of a number
+ * @n: string to print
+ * @a: input first digit
  * Return: square
  */
-look_multiples(int n, int a)
+int look_multiples(int n, int a)
 {
-    if (n <= 0)
-    {
-        return (-1);
-    }
-    if (a * a > n)
-    {
-        return (-1);
-    }
-    if (a * a == n)
-    {
-        return(a);
-    }
-    else
-    {
-        look_multiples(n, a + 1);
-    }
-    
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	if (a * a > n)
+	{
+		return (-1);
+	}
+	if (a * a == n)
+	{
+		return(a);		
+	}
+	else
+	{
+		look_multiples(n, a + 1);
+	}
 }
 int _sqrt_recursion(int n)
 {
-   int a = 1;
-   return(look_multiples(n, a));
+	int a = 1;
+	
+	return (look_multiples(n, a));
 }
