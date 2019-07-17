484a403b10b12d1e6fccae58f6141a689236cfd6
PauloMorillo <pauloan@hotmail.com>
2019-07-17 01:30:29 +0000

    added prototype for last exercise

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _holberton_h_
 #define _holberton_h_
 
+
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
