3980bb3310ebf48e4348aed70eec1f451f192593
PauloMorillo <pauloan@hotmail.com>
2019-07-19 00:12:09 +0000

    use other

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	p = malloc(new_size);
 	if (p == NULL)
+		free(p);
 		return (NULL);
 	for (i = 0; i < old_size && i < new_size; i++)
 		p[i] = ((char *)ptr)[i];
