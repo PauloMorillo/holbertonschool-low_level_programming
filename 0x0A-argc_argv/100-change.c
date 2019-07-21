33f5c38b3030c55df50c2808b87b48346bbb4784
PauloMorillo <pauloan@hotmail.com>
2019-07-21 17:58:49 +0000

    remove lines

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,60 +6,47 @@
  * @argv: input the array of arguments in commandline
  * Return: numbers of coins for change or null.
  */
-
 int main(int argc, char *argv[])
 {
 	int coin = 0, count, numb;
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-		return (0);
-	}
 	if (argc > 1 && argc < 3)
 	{
 		numb = atoi(argv[1]);
-
 		while (numb > 0)
 		{
 			if (numb >= 25)
 			{
 				coin = coin + 1;
-				numb = numb - 25;
-				count = 1;
+				count = 25;
 			}
 			else if (numb >= 10 && count == 0)
 			{
 				coin = coin + 1;
-				numb = numb - 10;
-				count = 1;
+				count = 10;
 			}
 			else if (numb >= 5 && count == 0)
 			{
 				coin = coin + 1;
-				numb = numb - 5;
-				count = 1;
+				count = 15;
 			}
 			else if (numb >= 2 && count == 0)
 			{
 				coin = coin + 1;
-				numb = numb - 2;
-				count = 1;
+				count = 2;
 			}
 			else if (numb >= 1 && count == 0)
 			{
 				coin = coin + 1;
-				numb = numb - 1;
 				count = 1;
 			}
+			numb = numb - count;
 			count = 0;
 		}
 		printf("%d\n", coin);
 	}
 	else
-	{
 		printf("Error\n");
 		return (1);
-	}
 	return (0);
 }
