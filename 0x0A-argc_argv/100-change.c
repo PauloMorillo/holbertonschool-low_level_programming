43f5a04cbbe36296735c9549f44732adfebdc3d4
PauloMorillo <pauloan@hotmail.com>
2019-07-21 17:47:14 +0000

    fixed with conditionals for coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,37 +4,57 @@
  * main - check the code for Holberton School students.
  * @argc: input the number or arguments in commandline
  * @argv: input the array of arguments in commandline
- * Return: Always 0.
+ * Return: numbers of coins for change or null.
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int suma;
-	int numb;
+	int coin = 0, count, numb;
 
 	if (argc == 1)
 	{
-		printf("%d\n", 0);
+		printf("Error\n");
 		return (0);
 	}
-	if (argc > 1)
+	if (argc > 1 && argc < 3)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			numb = atoi(argv[i]);
+		numb = atoi(argv[1]);
 
-			if ((numb == 0 && *argv[i] == '0') || numb > 0 || *argv[i] == '-')
+		while (numb > 0)
+		{
+			if (numb >= 25)
 			{
-				suma = suma + numb;
+				coin = coin + 1;
+				numb = numb - 25;
+				count = 1;
 			}
-			else
+			else if (numb >= 10 && count == 0)
 			{
-				printf("Error\n");
-				return (1);
+				coin = coin + 1;
+				numb = numb - 10;
+				count = 1;
 			}
+			else if (numb >= 5 && count == 0)
+			{
+				coin = coin + 1;
+				numb = numb - 5;
+				count = 1;
+			}
+			else if (numb >= 2 && count == 0)
+			{
+				coin = coin + 1;
+				numb = numb - 2;
+				count = 1;
+			}
+			else if (numb >= 1 && count == 0)
+			{
+				coin = coin + 1;
+				numb = numb - 1;
+				count = 1;
+			}
+			count = 0;
 		}
-		printf("%d\n", suma);
+		printf("%d\n", coin);
 	}
 	else
 	{
