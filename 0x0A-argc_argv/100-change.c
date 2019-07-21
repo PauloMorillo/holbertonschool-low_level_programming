f93c788c14732a64c1b7870ebc044d246215a1cd
PauloMorillo <pauloan@hotmail.com>
2019-07-21 18:04:47 +0000

    remove more lines

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,37 +16,25 @@ int main(int argc, char *argv[])
 		while (numb > 0)
 		{
 			if (numb >= 25)
-			{
-				coin = coin + 1;
 				count = 25;
-			}
 			else if (numb >= 10 && count == 0)
-			{
-				coin = coin + 1;
 				count = 10;
-			}
 			else if (numb >= 5 && count == 0)
-			{
-				coin = coin + 1;
 				count = 15;
-			}
 			else if (numb >= 2 && count == 0)
-			{
-				coin = coin + 1;
 				count = 2;
-			}
 			else if (numb >= 1 && count == 0)
-			{
-				coin = coin + 1;
 				count = 1;
-			}
+			coin = coin + 1;
 			numb = numb - count;
 			count = 0;
 		}
 		printf("%d\n", coin);
 	}
 	else
+	{
 		printf("Error\n");
 		return (1);
+	}
 	return (0);
 }
