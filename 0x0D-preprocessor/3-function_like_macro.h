283a2ea3bb57e618e9c9497a8b64085f2515e0fb
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-07-22 16:28:18 +0000

    fixed with include guards for double inclusion

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) (x >= 0 ? x: x * -1)
+#ifndef 3_function_like_macro_h_
+#define 3_function_like_macro_h_
+#define ABS(x) ((x) >= (0) ? (x) : (x) * (-1))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef 4_sum_h_
+#define 4_sum_h_
 #define SUM(x, y) ((x) + (y))
 #endif
