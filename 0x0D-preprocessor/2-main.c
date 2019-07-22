84ad1f571de3f7b3f9c87f832b58d3cfd729c33b
PauloMorillo <42702212+PauloMorillo@users.noreply.github.com>
2019-07-22 12:20:13 -0500

    fixed remove spaces

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/*#include _FILE_*/
 /**
  * main - print name of file with standard predefined macros
- *
  * Return: Always 0.
  */
 int main(void)
 {
 	printf(__FILE__);
 	printf("\n");
-    return (0);
+	return (0);
 }
