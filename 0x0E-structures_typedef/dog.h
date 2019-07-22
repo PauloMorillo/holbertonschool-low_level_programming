acf4a5d009efca1e6f1bcab3b62f75fd75fc5707
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-07-22 20:00:03 +0000

    created directory and exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Struct of elements
+ * @name: Dog's name
+ * @age: dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
