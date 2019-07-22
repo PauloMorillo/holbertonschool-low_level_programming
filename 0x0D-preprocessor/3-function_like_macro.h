224e267a1111f41ef7c5facc6c9f05ce5e15593c
PauloMorillo <42702212+PauloMorillo@users.noreply.github.com>
2019-07-22 12:13:49 -0500

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef 3_function_like_macro_h_
-#define 3_function_like_macro_h_
+#ifndef _3_function_like_macro_h_
+#define _3_function_like_macro_h_
 #define ABS(x) ((x) >= (0) ? (x) : (x) * (-1))
 #endif
