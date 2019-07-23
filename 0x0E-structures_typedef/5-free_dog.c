2a6fcf1bc4818f077fa139be2c8b5f2deac7f7c2
PauloMorillo <42702212+PauloMorillo@users.noreply.github.com>
2019-07-22 23:59:50 -0500

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * free_dog - initialize a variable of tyoe struct dog
- * @d: addres of structure
+ * free_dog - frees memory from allocated struct
+ * @d: input struct type
+ * Return: void function to free memory
  */
 void free_dog(dog_t *d)
 {
 	if (d)
 	{
-		free((*d).name);
-		free((*d).age);
-		free((*d).owner);
+		free(d->name);
+		free(d->owner);
+		free(d);
 	}
 }
