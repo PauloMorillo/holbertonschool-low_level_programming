bb0a24e5108a52c17c875ebdfdc4bea76a2f08e2
PauloMorillo <pauloan@hotmail.com>
2019-07-24 20:14:02 +0000

    created script exercise 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,39 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - searches for an interger
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to function to print double
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a = 0;
+	int b;
+	int pos;
+
+	if (cmp != NULL && array != NULL && size > 0)
+	{
+		while (a < size)
+		{
+			b = (*cmp)(array[a]);
+			if (b > 0)
+			{
+				pos = a;
+				break;
+			}
+			a = a + 1;
+		}
+		if (pos)
+		{
+			return (pos);
+		}
+		else
+		{
+			return (0);
+		}
+
+	}
+	return (-1);
+}
