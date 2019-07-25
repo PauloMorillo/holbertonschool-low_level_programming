7afef44be86b34aefd4c78015a2f7b27a1fcb700
PauloMorillo <pauloan@hotmail.com>
2019-07-25 03:23:56 +0000

    created script last exercise

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: input number of arguments in main
+ * @argv: input array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	char *p = (char *) &main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < *argv[1]; i++)
+	{
+		printf("%d\n", p[i] & 0xFF);
+	}
+	return (0);
+}
