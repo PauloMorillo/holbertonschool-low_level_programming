cedd43d738efa7bce46dc36ceae918ef15c15d92
PauloMorillo <pauloan@hotmail.com>
2019-07-26 18:48:39 +0000

    created script for exercise 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Function to sum all the arguments.
+ * @n: length of arguments
+ * Return: the sum of all or 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int suma = 0;
+	unsigned int i = 0;
+
+	va_start(valist, n);
+	while (i < n)
+	{
+		suma = suma + va_arg(valist, int);
+		i = i + 1;
+	}
+	va_end(valist);
+	return (suma);
+}
