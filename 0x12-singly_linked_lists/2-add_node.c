72570acee65f15addd154b704cbab42e5e98bb79
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-01 20:01:07 +0000

    fixed doing very simple

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,32 +10,18 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *s;
 	unsigned int leng = 0;
 	list_t *new;
-	unsigned int realleng = 1;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL || head == NULL)
 		return (NULL);
-	if (str == NULL)
-	{
-		str = strdup("(nil)");
-		realleng = 0;
-	}
-	s = malloc(sizeof(str) + 1);
-	if (s == NULL)
-		return (NULL);
 	while (str[leng] != '\0')
 	{
-		s[leng] = str[leng];
 		leng++;
 	}
-	s[leng] = '\0';
-	if (realleng != 0)
-		realleng = leng;
-	new->str = s;
-	new->len = realleng;
+	new->str = strdup(str);
+	new->len = leng;
 	new->next = *head;
 	*head = new;
 	return (new);
