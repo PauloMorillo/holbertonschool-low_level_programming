ac506d2fcdc3f409e36a20c2f24accf16c6b2495
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-02 01:51:47 +0000

    created script for exercise 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first - funtionc to print first to execute main
+ * Return: do not return
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
