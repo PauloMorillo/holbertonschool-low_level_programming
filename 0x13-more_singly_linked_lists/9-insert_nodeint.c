2989f32806e8b36364e0dfab038e8bc05b9ca976
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-06 23:37:03 +0000

    fixed with new pointer and conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,23 +3,48 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function to index a new element in a list
+ * insert_nodeint_at_index - function to add new node at a given position
  * @head: pointer to head of the list
- * @idx: position to index element
- * @n: element to index in list
- * Return: value of node according to index
+ * @idx: position to index the value
+ * @n: number to index in the list
+ * Return: value of node according to index or null if it fails
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int sum = 0;
+	unsigned int len = 0;
+	listint_t *p;
+	listint_t *new;
 
-	if (head == NULL)
-		return (0);
-	while (head != NULL)
+	if (*head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (NULL);
+	p = *head;
+	if (idx > 0)
 	{
-		sum = sum + head->n;
-		head = head->next;
+		while (p->next != NULL && len < idx - 1)
+		{
+			len = len + 1;
+			p = (p)->next;
+		}
+		if (len < idx - 1)
+			return (NULL);
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->next = p->next;
+		p->next = new;
+		new->n = n;
 	}
-	return (sum);
+	else
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->next = (*head)->next;
+		new->n = n;
+		*head = new;
+	}
+	return (new);
 }
