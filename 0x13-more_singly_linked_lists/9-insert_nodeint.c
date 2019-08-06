239b44f4bcdb1a4fbc6e0febc7211856acedf3dd
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-06 23:51:46 +0000

    fixed with address of pointer next

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int len = 0;
-	listint_t *p;
-	listint_t *new;
+	listint_t *p, *new;
 
 	if (*head == NULL)
 		return (NULL);
@@ -42,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
-		new->next = (*head)->next;
+		new->next = (*head);
 		new->n = n;
 		*head = new;
 	}
