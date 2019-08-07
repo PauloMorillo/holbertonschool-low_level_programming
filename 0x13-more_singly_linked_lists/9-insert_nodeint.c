82be5019c947459fbb9fb2482e3877e47124c147
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-07 00:49:25 +0000

    added condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,22 +27,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			len = len + 1;
 			p = (p)->next;
 		}
-		if (len < idx - 1 || len > idx - 1)
+		if (p == NULL || len < (idx - 1) || len > (idx - 1))
 			return (NULL);
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
+		new->n = n;
 		new->next = p->next;
 		p->next = new;
-		new->n = n;
 	}
 	else
 	{
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
-		new->next = (*head);
 		new->n = n;
+		new->next = (*head);
 		*head = new;
 	}
 	return (new);
