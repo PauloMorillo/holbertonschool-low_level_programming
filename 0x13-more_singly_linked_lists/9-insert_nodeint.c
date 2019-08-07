969d173fad9f4ec53b2bc700cdd9b0706174a9cd
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-07 03:59:53 +0000

    remove first conditions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,10 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int len = 0;
 	listint_t *p, *new;
 
-	if (*head == NULL)
-		return (NULL);
-	if ((*head)->next == NULL)
-		return (NULL);
 	p = *head;
 	if (idx > 0)
 	{
