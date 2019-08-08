46e65f4dbbee70e471ea7efe34ad8c6529627f96
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-08 23:29:50 +0000

    created script for exercise 2 and update header withprototypes

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * get_bit - Function to return the binary
+ * @index: position of bit
+ * @n: decimal to check in bits
+ * Return: Return value of bit or -1 if it fails
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int numc = 0;
+	int len = 0;
+
+	numc = n;
+	while ((numc >>= 1) > 0)
+	{
+		len++;
+	}
+	while (len >= 0)
+	{
+		if ((unsigned int) len == index)
+		{
+			if ((n >> len--) & 1)
+				return (1);
+			else
+				return (0);
+		}
+		len--;
+	}
+	return (-1);
+}
