352aec2cd2cd22d9f1e548996ce4ebdb40bf04cd
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-09 00:30:12 +0000

    fixed with remove conditions

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 32)
 		return (-1);
-	if ((n[0] >> index) | 1)
-		return ((*n) |= 1 << index);
-	else
-		return (-1);
-	return (-1);
+	return ((*n) |= 1 << index);
 }
