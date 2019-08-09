4a144bbf1527e1178ffa1c193e4cf9086be3288c
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-08-09 00:07:39 +0000

    fixed remove loops

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,26 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int numc = 0;
-	int len = 0;
-
-	numc = n;
 	if (index > 32)
 		return (-1);
-	while ((numc >>= 1) > 0)
-	{
-		len++;
-	}
-	while (len >= 0)
-	{
-		if ((unsigned int) len == index)
-		{
-			if ((n >> len--) & 1)
-				return (1);
-			else
-				return (0);
-		}
-		len--;
-	}
-	return (-1);
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
 }
