f6bdb4b943ceb4f52092ac4806ffaf650a26dea8
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-15 14:56:36 +0000

    fixed with good order

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,10 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		/* printf("%d\n", h->n); */
-		n++;
-		head = head->next;
 		if (n == index)
 			return (head);
+		head = head->next;
+		n++;
 	}
 	return (NULL);
 }
