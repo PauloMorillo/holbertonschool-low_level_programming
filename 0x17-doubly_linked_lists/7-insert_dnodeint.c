ca949b63a49e6839b45b95ac2ca5b0f5b96e98ee
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-15 21:00:59 +0000

    only add when is 0 an empty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	current = *h;
+	if (idx == 0 && *h ==NULL)
+	{
+		*h = new;
+		return (new);
+	}
 	if (idx == 0)
 	{
 		current->prev = new;
