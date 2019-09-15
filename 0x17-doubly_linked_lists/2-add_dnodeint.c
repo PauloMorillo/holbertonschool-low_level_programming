4465918875700d9a6ec71ffa2513a63f40a3c798
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-15 14:10:05 +0000

    fixed with condition when is the first element

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+	if (*head == NULL)
+	{
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->n = n;
+		(*head)->prev = new;
+		new->prev = NULL;
+		new->next = *head;
+		*head = new;
+	}
 	return (new);
 }
