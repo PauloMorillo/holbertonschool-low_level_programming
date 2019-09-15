f2f9ddd578b41c3d8786bd5a9889c608d9a5e76b
EC2 Default User <ec2-user@ip-172-31-91-220.ec2.internal>
2019-09-15 20:19:02 +0000

    fixed prev in the beginning

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	if (idx == 0)
 	{
+		current->prev = new;
 		new->next = current;
 		new->prev = prev;
 		*h = new;
