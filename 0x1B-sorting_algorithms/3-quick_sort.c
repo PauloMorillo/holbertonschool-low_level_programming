b65b393b7758946b3884ca158c67fb5aaaa66ac1
PauloMorillo <pauloan@hotmail.com>
2019-10-29 15:36:36 +0000

    fixed with equal 2

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -63,7 +63,7 @@ void quicksort(int *array, int lo, int hi, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array != NULL && size > 2)
+	if (array != NULL && size >= 2)
 	{
 		quicksort(array, 0, size - 1, size);
 	}
