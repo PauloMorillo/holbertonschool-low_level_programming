6c9913a2cd8e785e5546b85bb705ae3bb4ecfcb1
PauloMorillo <pauloan@hotmail.com>
2019-12-04 23:31:54 +0000

    added condition to compare the branches

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -15,15 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		if (tree->left)
-		{
-			printf("left%d\n%d\n", tree->n, a);
 			a = (1 + binary_tree_height(tree->left));
-		}
 		if (tree->right)
-		{
-			printf("right%d\n%d\n", tree->n, b);
 			b = (1 + binary_tree_height(tree->right));
-		}
 		if (a < b)
 			return (b);
 		else
