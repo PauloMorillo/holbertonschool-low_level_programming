fec7afb3603489ce183a55166e9099247c0359a2
PauloMorillo <pauloan@hotmail.com>
2019-12-04 23:29:08 +0000

    condition to choose the greater height

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,20 +1,34 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
+#include <stdio.h>
 /**
  * binary_tree_height - This function measures the height of a binary tree
  * @tree: root of tree
- * Return: It doesnt return anything
+ * Return: returns the height of the node
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int a = 0;
+	int b = 0;
+	/* int c = 0; */
+
 	if (tree)
 	{
 		if (tree->left)
-			return (1 + binary_tree_height(tree->left));
+		{
+			printf("left%d\n%d\n", tree->n, a);
+			a = (1 + binary_tree_height(tree->left));
+		}
 		if (tree->right)
-			return (1 + binary_tree_height(tree->right));
-
+		{
+			printf("right%d\n%d\n", tree->n, b);
+			b = (1 + binary_tree_height(tree->right));
+		}
+		if (a < b)
+			return (b);
+		else
+			return (a);
+		return (0);
 	}
 	return (0);
 }
