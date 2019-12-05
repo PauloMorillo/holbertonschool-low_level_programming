0d2c5b4b234c5d87faeb5f254c60c5edcd612021
PauloMorillo <pauloan@hotmail.com>
2019-12-05 17:17:27 +0000

    fixed taking the height

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 #include <stdio.h>
+size_t binary_tree_height_noroot(const binary_tree_t *tree);
 /**
  * binary_tree_balance - returns the balance (left - right) of the tree
  * @tree: root of tree
@@ -8,21 +9,35 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+
+	if (tree)
+	{
+		return (binary_tree_height_noroot(tree->left)
+			- binary_tree_height_noroot(tree->right));
+	}
+	return (0);
+}
+/**
+ * binary_tree_height_noroot - measures the height of a binary tree
+ * @tree: root of tree
+ * Return: returns the height of the node
+ */
+size_t binary_tree_height_noroot(const binary_tree_t *tree)
+{
+	int a = 1;
+	int b = 1;
 	/* int c = 0; */
-	int l = -1;
-	int r = -1;
 
 	if (tree)
 	{
 		if (tree->left)
-		{
-			l = (binary_tree_balance(tree->left));
-		}
+			a = (1 + binary_tree_height_noroot(tree->left));
 		if (tree->right)
-		{
-			r = (binary_tree_balance(tree->right));
-		}
-		return (l - r);
+			b = (1 + binary_tree_height_noroot(tree->right));
+		if (a < b)
+			return (b);
+		else
+			return (a);
 	}
 	return (0);
 }
